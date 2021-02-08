5ecade2aef9a1351395a66fac09f7fc4b5d868fc
Yahor-Pauliuchuk-Godel <y.pauliuchuk@godeltech.com>
2021-02-08 20:07:56 +0300

    Init

---
+++ LessonOne/Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LessonOne.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static IList<WeatherForecast> weatherForecasts
+            = new List<WeatherForecast>
+        {
+            new WeatherForecast
+            {
+                Date = new DateTime(2020, 02, 10),
+                TemperatureC = 20,
+                Summary = Summary.Warm,
+                City = "Brest"
+            },
+            new WeatherForecast
+            {
+                Date = new DateTime(2020, 02, 10),
+                TemperatureC = 14,
+                Summary = Summary.Mild,
+                City = "Vitebsk"
+            }
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(weatherForecasts);
+        }
+
+        [HttpPost]
+        public IActionResult Post(WeatherForecast newForecast)
+        {
+            weatherForecasts.Add(newForecast);
+
+            return Created(string.Empty, newForecast);
+        }
+    }
+}
